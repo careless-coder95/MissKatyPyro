66f24ba606b424b92d9f3e9b2b0dd2bf7e7db0f3
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-02-13 18:12:31 +0530

    Create start.sh to launch MissKaty application
    Initialize script to start MissKaty and manage sessions.
    
    Signed-off-by: careless-coder95 <dhananjaymaurya3000@gmail.com>

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+echo "Starting MissKaty..."
+
+# delete old sessions (very important)
+rm -f *.session
+rm -f *.session-journal
+rm -rf MissKaty.session
+rm -rf misskaty.session
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
 python3 update.py && python3 -m misskaty
